d54f0cd6b388b3889ce5b3fafbfa21426a7e13ed
MD Rashid Hussain <mdrashid.hussain.786.01@gmail.com>
2021-08-29 21:41:01 +0530

    index_page_04

--- public/frontendJs/script.js
+++ public/frontendJs/script.js
@@ -1,3 +1,6 @@
+// Footer Copyright Stuff
 const copyright = document.getElementById('footer-copyright')
 const date = new Date()
 copyright.innerHTML = `copyrights &copy; ${date.getFullYear()}, &nbsp;&nbsp; All rights reserved.`
+
+
